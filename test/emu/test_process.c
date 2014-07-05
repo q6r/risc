63814ded4c13880995ed53c160a57ed3973c66ca
q6r <qnix@0x80.org>
2014-07-05 02:06:38 -0700

    Added tests for almost all opcodes

--- test/emu/test_process.c
+++ test/emu/test_process.c
@@ -446,15 +446,19 @@ void test_op_xorib(void) {
          OP_XORIB, RR4, RR4, // rr4 = 0x00;
          );
     P_R_REPEAT(4, process_reg_immb);
+    // r1=0x18
+    // r2=0x18
+    // r3=0x28
+    // r4=0x28
     TEST_ASSERT_EQUAL_HEX(vm.regs.r1, 0x18);
     TEST_ASSERT_EQUAL_HEX(vm.regs.r2, 0x18);
     TEST_ASSERT_EQUAL_HEX(vm.regs.r3, 0x28);
     TEST_ASSERT_EQUAL_HEX(vm.regs.r4, 0x28);
     P_R_REPEAT(4, process_reg_immb);
-    TEST_ASSERT_EQUAL_HEX(vm.regs.r4, 0x00);
-    TEST_ASSERT_EQUAL_HEX(vm.regs.r1, 0x00);
-    TEST_ASSERT_EQUAL_HEX(vm.regs.r2, 0x00);
-    TEST_ASSERT_EQUAL_HEX(vm.regs.r1, 0x00);
+    TEST_ASSERT_EQUAL_HEX(vm.regs.r1, 0x18^0x18);
+    TEST_ASSERT_EQUAL_HEX(vm.regs.r2, 0x18^0x28);
+    TEST_ASSERT_EQUAL_HEX(vm.regs.r3, 0x28^0x28);
+    TEST_ASSERT_EQUAL_HEX(vm.regs.r4, 0x28^0x28);
 }
 
 void test_op_pushib(void) {
@@ -505,4 +509,213 @@ void test_op_subiw(void) {
     TEST_ASSERT_EQUAL_HEX(vm.regs.r1, ~0x1111+1);
     TEST_ASSERT_EQUAL_HEX(vm.regs.r2, ~0x2222+1);
     TEST_ASSERT_EQUAL_HEX(vm.regs.r3, ~0x3333+1);
+}
+
+void test_op_muliw(void) {
+    CODE (
+      OP_MOVIB, RR1, 0x01,
+         OP_MOVIB, RR2, 0x01,
+         OP_MOVIB, RR3, 0x01,
+         OP_MULIW, RR1, 0x11, 0x11,
+         OP_MULIW, RR2, 0x22, 0x22,
+         OP_MULIW, RR3, 0x33, 0x33,
+         );
+
+    P_R_REPEAT(3, process_reg_immb);
+    TEST_ASSERT_EQUAL_HEX(vm.regs.r1, 0x01);
+    TEST_ASSERT_EQUAL_HEX(vm.regs.r2, 0x01);
+    TEST_ASSERT_EQUAL_HEX(vm.regs.r3, 0x01);
+    P_R_REPEAT(3, process_reg_immw);
+    TEST_ASSERT_EQUAL_HEX(vm.regs.r1, 0x1111);
+    TEST_ASSERT_EQUAL_HEX(vm.regs.r2, 0x2222);
+    TEST_ASSERT_EQUAL_HEX(vm.regs.r3, 0x3333);
+}
+
+void test_op_diviw(void) {
+    CODE (
+      OP_MOVIB, RR1, 0x18,
+         OP_MOVIB, RR2, 0x18,
+         OP_MOVIB, RR3, 0x18,
+         OP_MOVIB, RR4, 0x18,
+         OP_DIVIW, RR1, 0x00, 0x04,
+         OP_DIVIW, RR2, 0x00, 0x04,
+         OP_DIVIW, RR3, 0x00, 0x04,
+         OP_DIVIW, RR4, 0x00, 0x04,
+         );
+    P_R_REPEAT(4, process_reg_immb);
+    TEST_ASSERT_EQUAL_HEX(vm.regs.r1, 0x18);
+    TEST_ASSERT_EQUAL_HEX(vm.regs.r2, 0x18);
+    TEST_ASSERT_EQUAL_HEX(vm.regs.r3, 0x18);
+    TEST_ASSERT_EQUAL_HEX(vm.regs.r4, 0x18);
+    P_R_REPEAT(4, process_reg_immw);
+    TEST_ASSERT_EQUAL_HEX(vm.regs.r1, 0x0006);
+    TEST_ASSERT_EQUAL_HEX(vm.regs.r2, 0x0006);
+    TEST_ASSERT_EQUAL_HEX(vm.regs.r3, 0x0006);
+    TEST_ASSERT_EQUAL_HEX(vm.regs.r4, 0x0006);
+}
+
+void test_op_xoriw(void) {
+    CODE (
+      OP_ADDIW, RR1, 0xaa, 0xaa,
+         OP_ADDIW, RR2, 0xbb, 0xbb,
+         OP_XORIW, RR1, 0x11, 0x11,
+         OP_XORIW, RR2, 0x22, 0x22,
+         );
+    P_R_REPEAT(2, process_reg_immw);
+    TEST_ASSERT_EQUAL_HEX(vm.regs.r1, 0xaaaa);
+    TEST_ASSERT_EQUAL_HEX(vm.regs.r2, 0xbbbb);
+    P_R_REPEAT(2, process_reg_immw);
+    TEST_ASSERT_EQUAL_HEX(vm.regs.r1, 0xbbbb);
+    TEST_ASSERT_EQUAL_HEX(vm.regs.r2, 0x9999);
+}
+
+
+void test_op_moviw(void) {
+    CODE (
+      OP_MOVIW, RR1, 0x11, 0x22, 
+         OP_MOVIW, RR2, 0x11, 0x22, 
+         OP_MOVIW, RR3, 0x11, 0x22, 
+         OP_MOVIW, RR4, 0x11, 0x22,
+         );
+    P_R_REPEAT(4, process_reg_immw);
+    TEST_ASSERT_EQUAL(vm.regs.r1, 0x1122);
+    TEST_ASSERT_EQUAL(vm.regs.r2, 0x1122);
+    TEST_ASSERT_EQUAL(vm.regs.r3, 0x1122);
+    TEST_ASSERT_EQUAL(vm.regs.r4, 0x1122);
+}
+
+void test_op_pushiw(void) {
+    CODE (
+      OP_PUSHIW, 0x11, 0x11,
+         OP_PUSHIW, 0x22, 0x22,
+         OP_PUSHIW, 0x33, 0x33,
+         OP_PUSHIW, 0x44, 0x44,
+         OP_POPW, RR1,
+         OP_POPW, RR2,
+         OP_POPW, RR3,
+         OP_POPW, RR4,
+         );
+
+    P_R_REPEAT(4, process_pushiw);
+    TEST_ASSERT_EQUAL_HEX(vm.regs.ps, 0x00000004);
+    P_R_REPEAT(4, process_popw);
+    TEST_ASSERT_EQUAL_HEX(vm.regs.ps, 0x00000000);
+
+    TEST_ASSERT_EQUAL_HEX(vm.regs.r1, 0x00004444);
+    TEST_ASSERT_EQUAL_HEX(vm.regs.r2, 0x00003333);
+    TEST_ASSERT_EQUAL_HEX(vm.regs.r3, 0x00002222);
+    TEST_ASSERT_EQUAL_HEX(vm.regs.r4, 0x00001111);
+    TEST_ASSERT_EQUAL_HEX(vm.regs.ps, 0x00000000);
+
+}
+
+void test_op_sub(void) {
+    CODE (
+      OP_MOVID, RR1, 0x11, 0x11, 0x11, 0x11,
+         OP_MOVID, RR2, 0x11, 0x11, 0x11, 0x11,
+         OP_MOVID, RR3, 0x22, 0x22, 0x22, 0x22,
+         OP_MOVID, RR4, 0x22, 0x22, 0x22, 0x22,
+         OP_SUB, RR1, RR2,
+         OP_SUB, RR3, RR4,
+         );
+    P_R_REPEAT(4, process_reg_immd);
+    TEST_ASSERT_EQUAL_HEX(vm.regs.r1, 0x11111111);
+    TEST_ASSERT_EQUAL_HEX(vm.regs.r2, 0x11111111);
+    TEST_ASSERT_EQUAL_HEX(vm.regs.r3, 0x22222222);
+    TEST_ASSERT_EQUAL_HEX(vm.regs.r4, 0x22222222);
+    P_R_REPEAT(2, process_reg_reg);
+    TEST_ASSERT_EQUAL_HEX(vm.regs.r1, 0x00000000);
+    TEST_ASSERT_EQUAL_HEX(vm.regs.r2, 0x11111111);
+    TEST_ASSERT_EQUAL_HEX(vm.regs.r3, 0x00000000);
+    TEST_ASSERT_EQUAL_HEX(vm.regs.r4, 0x22222222);
+}
+
+void test_op_movib(void) {
+    CODE (
+      OP_MOVIB, RR1, 0x11, 
+         OP_MOVIB, RR2, 0x11, 
+         OP_MOVIB, RR3, 0x11, 
+         OP_MOVIB, RR4, 0x11, 
+         );
+    P_R_REPEAT(4, process_reg_immb);
+    TEST_ASSERT_EQUAL_HEX(vm.regs.r1, 0x11);
+    TEST_ASSERT_EQUAL_HEX(vm.regs.r2, 0x11);
+    TEST_ASSERT_EQUAL_HEX(vm.regs.r3, 0x11);
+    TEST_ASSERT_EQUAL_HEX(vm.regs.r4, 0x11);
+}
+
+void test_op_mulid(void) {
+    CODE (
+      OP_MOVID, RR1, 0x11, 0x11, 0x11, 0x11,
+         OP_MOVID, RR2, 0x11, 0x11, 0x11, 0x11,
+         OP_MULID, RR1, 0x11, 0x11, 0x11, 0x11,
+         OP_MULID, RR2, 0x11, 0x11, 0x11, 0x11,
+         );
+    P_R_REPEAT(2, process_reg_immd);
+    TEST_ASSERT_EQUAL_HEX(vm.regs.r1, 0x11111111);
+    TEST_ASSERT_EQUAL_HEX(vm.regs.r2, 0x11111111);
+    P_R_REPEAT(2, process_reg_immd);
+    // should be 0x123456787654320
+    // but since we are in 32bit it
+    // should be 0x87654321 ??
+    TEST_ASSERT_EQUAL_HEX(vm.regs.r1, 0x87654321);
+    TEST_ASSERT_EQUAL_HEX(vm.regs.r2, 0x87654321);
+}
+
+void test_op_divid(void) {
+    CODE (
+      OP_MOVID, RR1, 0x11, 0x11, 0x11, 0x11,
+         OP_MOVID, RR2, 0x11, 0x11, 0x11, 0x11,
+         OP_DIVID, RR1, 0x11, 0x00, 0x00, 0x11,
+         OP_DIVID, RR2, 0x11, 0x00, 0x00, 0x11,
+         );
+    P_R_REPEAT(2, process_reg_immd);
+    TEST_ASSERT_EQUAL_HEX(vm.regs.r1, 0x11111111);
+    TEST_ASSERT_EQUAL_HEX(vm.regs.r2, 0x11111111);
+    P_R_REPEAT(2, process_reg_immd);
+    TEST_ASSERT_EQUAL_HEX(vm.regs.r1, 0x11111111/0x11000011);
+    TEST_ASSERT_EQUAL_HEX(vm.regs.r2, 0x11111111/0x11000011);
+}
+
+void test_op_xorid(void) {
+    CODE (
+      OP_MOVID, RR1, 0x11, 0x11, 0x11, 0x11,
+         OP_MOVID, RR2, 0x22, 0x22, 0x22, 0x22,
+         OP_XORID, RR1, 0xaa, 0xaa, 0xaa, 0xaa,
+         OP_XORID, RR2, 0xbb, 0xbb, 0xbb, 0xbb,
+         );
+    P_R_REPEAT(2, process_reg_immd);
+    TEST_ASSERT_EQUAL_HEX(vm.regs.r1, 0x11111111);
+    TEST_ASSERT_EQUAL_HEX(vm.regs.r2, 0x22222222);
+    P_R_REPEAT(2, process_reg_immd);
+    TEST_ASSERT_EQUAL_HEX(vm.regs.r1, 0x11111111^0xaaaaaaaa);
+    TEST_ASSERT_EQUAL_HEX(vm.regs.r2, 0x22222222^0xbbbbbbbb);
+}
+
+void test_op_pushid(void) {
+    CODE (
+      OP_PUSHID, 0x11, 0x11, 0x11, 0x11,
+         OP_PUSHID, 0x22, 0x22, 0x22, 0x22,
+         );
+    TEST_ASSERT_EQUAL_HEX(vm.regs.ps, 0x00000000);
+    P_R_REPEAT(2, process_pushid);
+    TEST_ASSERT_EQUAL_HEX(vm.regs.ps, 0x02);
+    TEST_ASSERT_EQUAL_HEX(vm.stack[0], 0x11111111);
+    TEST_ASSERT_EQUAL_HEX(vm.stack[1], 0x22222222);
+    TEST_ASSERT_EQUAL_HEX(vm.stack[2], 0x00000000);
+}
+
+void test_op_exit(void) {
+    TEST_IGNORE_MESSAGE("Implement me!");
+}
+
+void test_op_syscall(void) {
+    TEST_IGNORE_MESSAGE("Implement me!");
+}
+void test_op_gstk(void) {
+    TEST_IGNORE_MESSAGE("Implement me!");
+}
+
+void test_op_pstk(void) {
+    TEST_IGNORE_MESSAGE("Implement me!");
 }
